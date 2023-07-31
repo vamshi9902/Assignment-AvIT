bbfd87a1cb2806895a1e376c1ee6a7a22156652d
vamshi <vamshikrishna9902@gmail.com>
2023-07-31 19:10:33 +0530

    CommitedFiles

--- Bill-Splitter/app.js
+++ Bill-Splitter/app.js
@@ -4,25 +4,29 @@ function billSplit(){
 
     let result = amount/people;
 
-    document.getElementById("resultAmountt").value = "Each person should pay : " + result.toFixed(2);
+    document.getElementById("resultAmountt").value = result.toFixed(2);
 
     document.getElementById("billAmount").value = " ";
     document.getElementById("persons").value = " ";
 
     if(amount == 0 || amount == " "){
-        document.getElementById("alertmsg1").innerText = "Enter correct value"
+        document.getElementById("alertmsg1").innerText = "Enter correct value";
         document.getElementById("billAmount").value = "";
         document.getElementById("persons").value = people;
-        document.getElementById("resultAmountt").value = " "
+        document.getElementById("resultAmountt").value = " ";
+        document.getElementById("billAmount").style.borderColor="red";
     } 
     else if(people == 0 || people == " "){
-        document.getElementById("alertmsg2").innerText = "Enter correct value"
+        document.getElementById("alertmsg2").innerText = "Enter correct value";
         document.getElementById("persons").value = "";
         document.getElementById("billAmount").value = amount;
-        document.getElementById("resultAmountt").value = " "                    
+        document.getElementById("resultAmountt").value = " " ;
+        document.getElementById("persons").style.borderColor="red";                  
     }
     else{
-        document.getElementById("alertmsg1").innerText = " "
-        document.getElementById("alertmsg2").innerText = " "
+        document.getElementById("alertmsg1").innerText = " ";
+        document.getElementById("alertmsg2").innerText = " ";
+        document.getElementById("billAmount").style.borderColor="black";
+        document.getElementById("persons").style.borderColor="black";
     }
 }

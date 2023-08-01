5bf15a9d4d3e0ab076b1f8a83957f65b62ed0166
vamshi <vamshikrishna9902@gmail.com>
2023-08-01 19:15:41 +0530

    Tax_Calculator

--- Tax-Calculator/app.js
+++ Tax-Calculator/app.js
@@ -9,19 +9,19 @@ function taxconverter(name,salary){
         document.getElementById("result").value = `${name} ! You are Tax free`;
     }
     else if(salary <= 600000){
-        ded1 = 30000;
+        ded1 = 0;
         ded2 = (salary-300000)*0.1;
-        document.getElementById("ded1").value = ded1;
         totalTax = ded1 + ded2;
+        document.getElementById("ded1").value = ded1;
         document.getElementById("ded2").value = ded2;
         document.getElementById("totalded").value = totalTax;
         document.getElementById("result").value = `${name} ! Your Tax amount = ${totalTax}`;   
     }
     else if(salary > 600000){
-        ded1 = 60000;
+        ded1 = 30000;
         ded2 = (salary-600000)*0.2;
-        document.getElementById("ded1").value = ded1;
         totalTax = ded1 + ded2;
+        document.getElementById("ded1").value = ded1;
         document.getElementById("ded2").value = ded2;
         document.getElementById("totalded").value = totalTax;
         document.getElementById("result").value = `${name} ! Your Tax amount is: ${totalTax}`;
